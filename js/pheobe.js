a7a00ff8b80b177cfe2e2a4744d3e83506d95e70
Piotr Sobusiak <piotr.sobusiak@applover.pl>
2016-08-27 17:33:07 +0200

    firebase configuration, html and css

---
+++ js/pheobe.js
@@ -0,0 +1,11 @@
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD3BTDP0LPGxiq-N2OxADnisGF1mDgUw4c",
+  authDomain: "quick-beer.firebaseapp.com",
+  databaseURL: "https://quick-beer.firebaseio.com",
+  storageBucket: "quick-beer.appspot.com",
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
